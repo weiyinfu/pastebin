9061e45db24dd7d92364debd4535bf254c1c39e0
weiyinfu <wei.yinfu@qq.com>
2020-02-29 13:58:50 +0800

    good

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -63,16 +63,7 @@ const webpackConfig = {
     new CopyWebpackPlugin([
       {
         from: path.join(__dirname, "./public")
-      }, {
-        from: 'node_modules/mavon-editor/dist/highlightjs',
-        to: path.resolve(__dirname, 'dist/highlightjs'), // 插件将会把文件导出于/dist/highlightjs之下
-      }, {
-        from: 'node_modules/mavon-editor/dist/markdown',
-        to: path.resolve(__dirname, 'dist/markdown'), // 插件将会把文件导出于/dist/markdown之下
-      }, {
-        from: 'node_modules/mavon-editor/dist/katex', // 插件将会把文件导出
-        to: path.resolve(__dirname, 'dist/katex')
-      }
+      },
     ]),
     {
       condition: production,
